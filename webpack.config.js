8efe27ed6201d46ad4358ea10cf92ac404c4fdef
alexzhong22c <535377683@qq.com>
2017-09-28 17:19:27 +0800

    // 为下一章做回退的处理

--- webpack.config.js
+++ webpack.config.js
@@ -6,23 +6,6 @@ const config = {
     filename: 'bundle.js',
     // path: __dirname + '/dist'
     path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/, // 匹配由.css结尾的文件
-        use: [ // 使用到的npm包
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
   }
 }
 module.exports = config
