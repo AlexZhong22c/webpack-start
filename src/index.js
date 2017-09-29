a7964527c2113c56aa48a74d4279309168a05f14
alexzhong22c <535377683@qq.com>
2017-09-29 13:39:51 +0800

    问题：vendor的hash不应该地变化
    这是因为每个 module.id 会基于默认的解析顺序(resolve order)进行增量：
    我们引入了print.js后，在命令行里看到了为print.js分配了一个module.id。
    因此vendor的module.id发生了变化。

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash'
+import Print from './print'
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!')
@@ -8,9 +9,9 @@ function component () {
   var element = document.createElement('div')
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.onClick = Print.bind(null, 'Hello webpack!')
   return element
 }
 
 document.body.appendChild(component())
-
 
