5b29fc6532614f466ec37aedfd991e905a651349
alexzhong22c <535377683@qq.com>
2017-09-29 10:14:21 +0800

    代码分离 - 动态导入import
    对于动态导入，第一种，也是优先选择的方式是，使用符合 ECMAScript 提案 的 import() 语法。第二种，则是使用 webpack
    特定的 require.ensure。让我们先尝试使用第一种……

--- src/another-module.js
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash'
-function anotherC () {
-  var element = document.createElement('pre')
-  var _loadedStr = _.join(['Another', 'module', 'loaded!'], ' ')
-  element.innerHTML = _loadedStr
-  return element
-}
-
-document.body.appendChild(anotherC())
--- webpack.common.js
+++ webpack.common.js
@@ -1,24 +1,20 @@
 const path = require('path')
-const webpack = require('webpack')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const config = {
   entry: {
-    app: './src/index.js',
-    another: './src/another-module.js'
+    app: './src/index.js'
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'common' // 指定公共 bundle 的名称。
-    }),
     new HtmlWebpackPlugin({
       title: 'Code Splitting'
     })
   ],
   output: {
     filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 }
