32fcc336fb70dec6ef46b8fe94d3f0a74147dc1c
alexzhong22c <535377683@qq.com>
2017-09-29 13:10:55 +0800

    缓存 - 提取模板
    CommonsChunkPlugin能够在每次修改后的构建结果中，将 webpack 的样板(boilerplate)和 manifest
    提取出来。（这两个内容经常会导致hash变化）
    另外，将长久不变的第三方库提取到vendor中，提高缓存复用vendor这部分文件的几率。

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,25 @@
 const path = require('path')
+const webpack = require('webpack')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const config = {
   entry: {
-    app: './src/index.js'
+    app: './src/index.js',
+    vendor: [
+      'lodash'
+    ]
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Caching'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'runtime'
     })
   ],
   output: {
